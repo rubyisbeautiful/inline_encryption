447a76acf2f54c0429a209f053b8b1b958973bfb
rubyisbeautiful <bryan.taylor@sabre.com>
2014-01-23 12:13:24 -0600

    remove requirement for cli in top level module suppress i18n warning

--- lib/inline_encryption.rb
+++ lib/inline_encryption.rb
@@ -4,7 +4,6 @@ require 'inline_encryption/version'
 require 'inline_encryption/config'
 require 'inline_encryption/base'
 require 'inline_encryption/errors'
-require 'inline_encryption/cli'
 
 module InlineEncryption
   extend InlineEncryption::Base
@@ -12,6 +11,7 @@ module InlineEncryption
   unless @_i18n_initialized_for_ie
     locale_file = File.expand_path(File.join(File.dirname(__FILE__), '../config/locales.yml'))
     I18n.load_path << locale_file
+    I18n.enforce_available_locales = false
     @_i18n_initialized_for_ie = true
   end
 
